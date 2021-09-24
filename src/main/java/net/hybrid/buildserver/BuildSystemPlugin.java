f51798e1488dc4f972f6e4260ad6e44979e4da8b
liam <71061493+LiamHBest0608@users.noreply.github.com>
2021-09-24 16:43:07 +0200

    First

---
+++ src/main/java/net/hybrid/buildserver/BuildSystemPlugin.java
@@ -0,0 +1,17 @@
+package net.hybrid.buildserver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BuildSystemPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
+}
